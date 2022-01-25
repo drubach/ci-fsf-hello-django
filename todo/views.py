478ad8046e8fca5b1d3b26f859105eb3c40e7200
drubach <daniel.c.rubach@gmail.com>
2022-01-24 20:22:59 -0500

    added todo template and ran migrations, also setup superuser

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,8 @@
+'''Views functions'''
+#from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def get_todo_list(request):
+    '''Get todo list'''
+    return render(request, 'todo/todo_list.html')
